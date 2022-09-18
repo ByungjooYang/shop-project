b94f3922007d51ab3f1fd4382b81cb7c1cecc2df
byungjoo <byungjoo104@gmail.com>
2022-09-18 22:44:51 +0900

    first commit

---
+++ src/layouts/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Soldout</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">홈</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link active" to="/">제품리스트</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/detail">제품상세페이지</router-link>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
