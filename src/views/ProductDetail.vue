85c811e96e7180144060952bd6b89ed5bab83d6c
byungjoo <byungjoo104@gmail.com>
2022-09-26 23:17:03 +0900

    update product detail

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,5 +1,32 @@
 <template>
-
+    <main class="mt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div id="carouselExampleIndicators" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+                            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div :class="`carousel-item ${i===0?'active':''}`" :key="i" v-for="(pimg, i) in productImage">
+                                <img :src="`/download/${productId}/${pimg.path}`" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -35,8 +62,14 @@ export default {
             let productDetail = await this.$api("/api/productDetail", {param: [this.productId]});
             if(productDetail.length > 0){
                 this.productDetail = productDetail[0];
+                this.totalPrice = this.totalPrice = this.productDetail.product_price * this.total;
 
             }
+            console.log(this.productDetail);
+        },
+        async getProductImage(){
+            this.productImage = await this.$api("/api/productMainImages", {param: [this.productId]});
+            console.log("this.productImage", this.productImage);
         }
     }
 }
